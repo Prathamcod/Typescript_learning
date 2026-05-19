cddac8b4129bb1a878bae49cc3ac26d472008960
Prathamcod <prathamsuthar384@gmail.com>
2026-05-19 11:19:40 +0530

    basic typescript and set up

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let username = "Pratham";
+console.log(username);
+let age = 21;
+console.log(age);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let username: string = "Pratham";
+
+console.log(username);
+
+let age: number = 21;
+console.log(age)
